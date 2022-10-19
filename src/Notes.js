37b2c3dc80a88901f0f7942f54979d57fdfa46ff
Anshuman Bisoyi <71143398+anshumanbisoyi@users.noreply.github.com>
2022-10-20 02:35:23 +0530

    Update Notes.js
    This was made for testing purposes.

--- src/Notes.js
+++ src/Notes.js
@@ -23,5 +23,11 @@ const Notes = [
       content:
         "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
     }
+    {
+      key: 5,
+      title: "DevOps",
+      content:
+        "DevOps is a set of practices that combines software development and IT operations. It aims to shorten the systems development life cycle and provide continuous delivery with high software quality. "
+    }
   ];
   export default Notes;
